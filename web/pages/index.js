c9def527473890ef0d263e71ed2b75cd50ba67ff
Manu <mcabral@frba.utn.edu.ar>
2021-08-29 11:37:55 -0300

    Cambios yarn/npm y index.js base de la página

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,91 @@
-import Link from "next/link";
-export default function Inicio() {
+import useSwr from "swr";
+import styled from "styled-components";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+const Main = styled.div`
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.backgrounds.primary};
+`;
+
+const Content = styled.div`
+  display: flex;
+  padding: 1rem;
+
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.backgrounds.secondary};
+`;
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #e80000;
+`;
+
+const Subtitle = styled.h2`
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+const Description = styled.p`
+  color: ${({ theme }) => theme.colors.secondary};
+`;
+
+const ContentStats = styled.p`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding-top: 1rem;
+  justify-content: space-around;
+
+  @media screen and (max-width: 720px) {
+    flex-direction: column;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+`;
+
+const Item = styled.p`
+  padding: 1rem;
+  &:hover {
+    cursor: pointer;
+    background-color: ${({ theme }) => theme.backgrounds.secondary};
+  }
+  //border: 2px solid #e80000;
+`;
+
+export default function Home() {
+  const { data, error } = useSwr("/api/covid", fetcher);
+
+  if (error) return <Title>Error al cargar.</Title>;
+  if (!data) return <Title>Cargando..</Title>;
+
   return (
-    <div>
-      <div>Codavi</div>
-      <Link href="/info" as={process.env.BACKEND_URL + "/info"}>
-        <a>Sobre el proyecto</a>
-      </Link>
-    </div>
+    <Main>
+      <Content>
+        <Title>CODAVI</Title>
+        <Description>
+          Visualización y estadísticas sobre el COVID-19 en toda la Argentina.
+        </Description>
+      </Content>
+      <ContentStats>
+        <Item>
+          <Subtitle>Mujeres vs Hombres</Subtitle>
+          <Description>
+            Cantidad de vacunados por género solamente en la primera dosis.
+          </Description>
+        </Item>
+        <Item>
+          <Subtitle>Comparativa de vacunas</Subtitle>
+          <Description>
+            Cantidad aplicada de cada vacuna (marca) en la primera dosis.
+          </Description>
+          <Title>54950</Title>
+        </Item>
+      </ContentStats>
+    </Main>
   );
 }
