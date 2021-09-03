f2e04ab1417d5be64394e13c49dc06b30869d056
Manu <mcabral@frba.utn.edu.ar>
2021-09-03 20:33:11 -0300

    Actualización de la página web.

---
+++ web/components/Card.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const MainCard = styled.div`
+  padding: 1rem;
+  //border: 2px solid #e80000;
+`;
+
+const CardBody = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding-top: 1rem;
+  background-color: ${({ theme }) => theme.backgrounds.secondary};
+  justify-content: space-around;
+`;
+
+const CardBodyItem = styled.div`
+  padding: 1rem;
+`;
+
+const Title = styled.h1`
+  font-size: 28px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+const Subtitle = styled.h2`
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+const Value = styled.h2`
+  color: #e80000;
+`;
+
+const Description = styled.p`
+  color: ${({ theme }) => theme.colors.secondary};
+`;
+
+export default function Card({ title, description, data }) {
+  var [values] = useState(data);
+  return (
+    <MainCard>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+      <CardBody>
+        {values.map((e) => (
+          <CardBodyItem>
+            <Subtitle>{e.nombre}</Subtitle>
+            <Value>{new Intl.NumberFormat().format(e.total)}</Value>
+          </CardBodyItem>
+        ))}
+      </CardBody>
+    </MainCard>
+  );
+}
---
+++ web/pages/index.js
@@ -0,0 +1,132 @@
+import styled from "styled-components";
+import Card from "../components/Card";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function Home({ vacunas, gpd, gsd }) {
+  return (
+    <Main>
+      <Container>
+        <Title>CODAVI</Title>
+        <Description>
+          Visualización y estadísticas sobre el COVID-19 en toda la Argentina.
+        </Description>
+      </Container>
+      <Subcontainer>
+        <Content>
+          <Card
+            title={vacunas["titulo"]}
+            description={vacunas["descripcion"]}
+            data={Object.values(vacunas["data"])}
+          />
+        </Content>
+        <Content>
+          <Card
+            title={gpd["titulo"]}
+            description={gpd["descripcion"]}
+            data={Object.values(gpd["data"])}
+          />
+          <Card
+            title={gsd["titulo"]}
+            description={gsd["descripcion"]}
+            data={Object.values(gsd["data"])}
+          />
+        </Content>
+      </Subcontainer>
+      <Container>
+        <Description>&copy; Copyright 2021 - Manuel Cabral</Description>
+      </Container>
+    </Main>
+  );
+}
+
+export async function getStaticProps(context) {
+  const res = await fetch(`http://localhost:5000/vacunas`);
+  const resgpd = await fetch(`http://localhost:5000/genero/1`);
+  const resgsd = await fetch(`http://localhost:5000/genero/2`);
+  const gpd = await resgpd.json();
+  const gsd = await resgsd.json();
+  const vacunas = await res.json();
+
+  if (!vacunas) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: vacunas,
+      },
+    };
+  }
+
+  if (!gpd) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: gpd,
+      },
+    };
+  }
+
+  if (!gsd) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: gsd,
+      },
+    };
+  }
+
+  return {
+    props: { vacunas, gpd, gsd },
+  };
+}
+
+const Main = styled.div`
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.backgrounds.primary};
+`;
+
+const Container = styled.div`
+  display: flex;
+  padding: 1rem;
+
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.backgrounds.secondary};
+`;
+
+const Subcontainer = styled.div`
+  display: flex;
+  padding: 1rem;
+  padding-bottom: 4rem;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.backgrounds.primary};
+`;
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #e80000;
+`;
+
+const Description = styled.p`
+  color: ${({ theme }) => theme.colors.secondary};
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding-top: 1rem;
+  justify-content: space-around;
+
+  @media screen and (max-width: 720px) {
+    flex-direction: column;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+`;
