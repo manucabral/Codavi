66ef03703fffa118a71aacefddf14ec2eec6e100
Manu <mcabral@frba.utn.edu.ar>
2021-09-03 04:43:54 -0300

    Agregado actualizador para datos sobre g/vacunacion

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from urllib.request import urlopen
+from datetime import datetime
+import pandas as pd
+import mysql.connector
 import zipfile
 import shutil
 import os
 
-# Ubicación de los datasets oficiales del gobierno.
+FECHA_ACTUAL = datetime.now().date().isoformat()
 URL_DATASET = 'https://sisa.msal.gov.ar/datos/descargas/covid-19/files/'
-
-# Archivo que contiene las vacunaciones.
 VACUNACION_ARCHIVO = 'datos_nomivac_covid19'
-
-# Archivo que contiene los casos.
 CASOS_ARCHIVO = 'Covid19Casos'
 
 def existeArchivo(nombreArchivo):
@@ -35,6 +34,41 @@ def descargarArchivo(nombreArchivo):
 
     return print('Archivo', csv, 'descargado correctamente en el directorio.')
 
+def actualizarDatosGenero():
+    masculinos = [0, 0]
+    femeninos = [0, 0]
+
+    try:
+        data = pd.read_csv('datos_nomivac_covid19.csv', usecols=[
+                           'orden_dosis', 'sexo'], skipinitialspace=True)
+    except Exception as Error:
+        return Error
+
+    for i in range(2):
+        femeninos[i] = data.query(
+            f'orden_dosis == {i+1} and sexo == "F"').sexo.count()
+        masculinos[i] = data.query(
+            f'orden_dosis == {i+1} and sexo == "M"').sexo.count()
+
+    cnx = mysql.connector.connect(
+        host="unknown",
+        port=3306,
+        user="unknown",
+        password="unknown",
+        db="unknown"
+    )
+    cursor = cnx.cursor()
+
+    for i in range(2):
+        query = (
+            f"INSERT INTO `unknown`.`generos` (`masculino`, `femenino`, `dosis`, `fecha`) VALUES ({masculinos[i]}, '{femeninos[i]}', '{i+1}', '{FECHA_ACTUAL}');")
+        cursor.execute(query)
+    
+    cursor.close()
+    cnx.commit()
+    cnx.close()
+    return "Datos de generos actualizados correctamente"
+
 if __name__ == "__main__":
 
     try:
@@ -50,4 +84,5 @@ if __name__ == "__main__":
 
     except ValueError:
         print('Ocurrio un error al descargar los archivos: ', ValueError)
-        
+
+    actualizarDatosGenero()
