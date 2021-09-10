a909ceda7e3c353cded97088b2d3b5da7de7dc33
Manu <mcabral@frba.utn.edu.ar>
2021-09-10 00:21:21 -0300

    Agregado vacunados por género en la DOSIS 1.

--- codavi.py
+++ codavi.py
@@ -1,6 +1,7 @@
 
 from data.descargar import descargarArchivo, existeArchivo, CASOS_ARCHIVO, VACUNACION_ARCHIVO
-from data.scripts import totalDosisAplicadas
+from data.scripts.totalDosisAplicadas import obtenerTotalDosis
+from data.scripts.comparativaGenero import obtenerComparativaGenero
 
 if __name__ == "__main__":
     try:
@@ -9,12 +10,17 @@ if __name__ == "__main__":
         else:
             print('Archivo encontrado:', VACUNACION_ARCHIVO)
 
-        #if not existeArchivo(CASOS_ARCHIVO):
+        # if not existeArchivo(CASOS_ARCHIVO):
         #    descargarArchivo(CASOS_ARCHIVO)
-        #else:
+        # else:
         #    print('Archivo encontrado:', CASOS_ARCHIVO)
 
     except ValueError:
         print('Ocurrio un error al descargar los archivos: ', ValueError)
 
-    totalDosisAplicadas()
+    total_dosis = obtenerTotalDosis()
+    print(f'Total de dosis suministradas hasta hoy: {total_dosis:,}')
+    total_masculinos, total_femeninos = obtenerComparativaGenero(1)
+    print('> DOSIS 1')
+    print(
+        f'Personas masculinas vacunadas: {total_masculinos:,}\nPersonas femeninas vacunadas: {total_femeninos:,}')
