3d6ee7031584ad9b8a8ac39f1ee923064ec36fd4
Manu <mcabral@frba.utn.edu.ar>
2021-09-10 00:21:34 -0300

    Agregado script para obtener los vacunados por género.

---
+++ data/scripts/comparativaGenero.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+def obtenerComparativaGenero(dosis):
+    data = pd.read_csv(os.getcwd() + '/datos_nomivac_covid19.csv', usecols = ['orden_dosis', 'sexo'])
+    masculinos = data.query(f'orden_dosis == {dosis} and sexo == "M"').sexo.count()
+    femeninos = data.query(f'orden_dosis == {dosis} and sexo == "F"').sexo.count()
+    return masculinos, femeninos
