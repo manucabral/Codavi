4b5fb0bcb20a68e50c6ec5437ae9f95498d1035d
Manu <mcabral@frba.utn.edu.ar>
2021-12-27 22:09:21 -0300

    Actualizado reporte batchfile

--- reportes/reporte.bat
+++ reportes/reporte.bat
@@ -1,10 +1,10 @@
 py descargar.py
 "C:\Program Files\7-Zip\7z.exe" e "Covid19Casos.zip"
 py reporte.py
+del /f Covid19Casos.csv
+del /f Covid19Casos.zip
 git pull
 git add *.csv
 git commit -m "Reporte para el dia de hoy"
 git push
-del /f Covid19Casos.csv
-del /f Covid19Casos.zip
 pause
