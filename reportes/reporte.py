999bb37a3fc7ab9a68581de22cbc39631c4a0d43
manucabral <83357673+manucabral@users.noreply.github.com>
2021-12-29 13:44:06 -0300

    Update reporte.py

--- reportes/reporte.py
+++ reportes/reporte.py
@@ -6,29 +6,63 @@
 from datetime import datetime
 import os, pandas as pd
 
-print('>> Analizando los datos..')
-hoy = datetime.now().strftime('%Y-%m-%d')
-data = pd.read_csv('./Covid19Casos.csv', usecols = ['clasificacion_resumen', 'fallecido', 'sexo'])
+class CodaviReporte:
 
-print('>> Filtrando a los casos confirmados..')
-filtro = data.query('clasificacion_resumen == "Confirmado"')
+    def obtener_fecha(self) -> str:
+        self.hoy = datetime.now().strftime('%Y-%m-%d')
+        return self.hoy
+    
+    def leer_dataset(self, archivo: str, columnas: list) -> bool:
+        try:
+            print(f'> Leyendo el archivo {archivo.split(".")[0]}...')
+            self.data = pd.read_csv(archivo, usecols=columnas)
+            print('>> Lectura terminada')
+        except Exception as e:
+            print('Ocurrio un error al leer el dataset', e)
+            return False
+    
+    def filtrar(self) -> bool:
+        try:
+            print('> Filtrando los datos...')
+            self.data = self.data.query('clasificacion_resumen == "Confirmado"')
+            print('>> Filtrado terminado')
+            return True
+        except Exception as e:
+            print('Ocurrio un error al filrar el dataset', e)
+            return False
 
-print('>> Obteniendo los campos..')
-total_confirmados = filtro.count()[0]
-total_fallecidos = filtro.query('fallecido == "SI"').count()[0]
-confirmados_masculinos = filtro.query('sexo == "M"').count()[0]
-confirmados_femeninos = filtro.query('sexo == "F"').count()[0]
-fallecidos_masculinos = filtro.query('fallecido == "SI" and sexo == "M"').count()[0]
-fallecidos_femeninos = filtro.query('fallecido == "SI" and sexo == "F"').count()[0]
+    def generar_casos(self) -> None:
+        total_confirmados = self.data.count()[0]
+        confirmados_masculinos = self.data.query('sexo == "M"').count()[0]
+        confirmados_femeninos = self.data.query('sexo == "F"').count()[0]
 
-print('>> Generando reporte de casos..')
-with open('confirmados.csv', 'a') as f:
-    f.write(f'{hoy},{total_confirmados},{confirmados_masculinos},{confirmados_femeninos}\n')
-    f.close()
-print('>> Reporte de casos finalizado.')
+        print('> Generando reporte de casos...')
+        try:
+            with open('confirmados.csv', 'a') as f:
+                f.write(f'{self.hoy},{total_confirmados},{confirmados_masculinos},{confirmados_femeninos}\n')
+                f.close()
+            print('>> Reporte de casos finalizado')
+        except Exception as e:
+            print('Ocurrio un error al generar el reporte', e)
+    
+    def generar_fallecidos(self) -> None:
+        total = self.data.query('fallecido == "SI"').count()[0]
+        masculinos = self.data.query('fallecido == "SI" and sexo == "M"').count()[0]
+        femeninos = self.data.query('fallecido == "SI" and sexo == "F"').count()[0]
 
-print('>> Generando reporte de fallecidos..')
-with open('fallecidos.csv', 'a') as f:
-    f.write(f'{hoy},{total_fallecidos},{fallecidos_masculinos},{fallecidos_femeninos}\n')
-    f.close()
-print('>> Reporte de fallecidos finalizado.')
+        print('> Generando reporte de fallecidos...')
+        try:
+            with open('fallecidos.csv', 'a') as f:
+                f.write(f'{self.hoy},{total},{masculinos},{femeninos}\n')
+                f.close()
+            print('>> Reporte de fallecidos finalizado')
+        except Exception as e:
+            print('Ocurrio un error al generar el reporte', e)
+
+if __name__ == "__main__":
+    reporte = CodaviReporte()
+    reporte.obtener_fecha()
+    reporte.leer_dataset('Covid19Casos.csv', ['clasificacion_resumen', 'fallecido', 'sexo'])
+    reporte.filtrar()
+    reporte.generar_casos()
+    reporte.generar_fallecidos()
